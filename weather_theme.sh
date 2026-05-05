340a3bc4eafed9472b3b0cd8530c5a02df24723b
SimonB <simonkgberg03@gmail.com>
2026-05-05 04:30:19 +0200

    stupid capitalizer

--- weather_theme.sh
+++ weather_theme.sh
@@ -3,9 +3,9 @@
 # init.. test
 
 # grabber
-DATA = $(curl -s "wttr.in/oslo=format=j1")
-TEMP = $(echo $DATA | jq -r ".xurrent_condition[0].temp_C")
-DESC = $(echo | jq -r ".current_condition[0].weatherDesc[0].value" | tr "[:upper]" "[:lower]")
+DATA=$(curl -s "wttr.in/oslo=format=j1")
+TEMP=$(echo $DATA | jq -r ".xurrent_condition[0].temp_C")
+DESC=$(echo | jq -r ".current_condition[0].weatherDesc[0].value" | tr "[:upper]" "[:lower]")
 
 # mapping
 
